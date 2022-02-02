78b72f0280b052f37d665afb66dab4c318c2d13a
tomek <luci6969@protonmail.ch>
2022-02-02 10:11:34 +0100

    little change with selenium, adding .env and change telegram module

--- lucky_nano_bot.py
+++ lucky_nano_bot.py
@@ -1,27 +1,33 @@
-import time
-import json
-import random
-import telegram_send
+import os
 import platform
+import random
+import time
 from datetime import datetime
+import telepot
+from dotenv import load_dotenv, find_dotenv
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def send_notification(message):
-    telegram_send.send(messages=[f"{message}"])
+    api_bot = os.environ.get("api_bot")
+    chat_id = os.environ.get("chat_id")
+    bot = telepot.Bot(api_bot)
+    bot.sendMessage(chat_id, message)
 
 
 def get_lottery_time():
-    chat = driver.find_element_by_css_selector("#chat-input")
+    chat = driver.find_element(By.CSS_SELECTOR, "#chat-input")
     chat.send_keys("/lottery")
     chat.send_keys(Keys.RETURN)
-    lottery_time = driver.find_elements_by_class_name("chat_system_message")
+    lottery_time = driver.find_elements(By.CLASS_NAME, "chat_system_message")
     send_notification(lottery_time[-1].text)
     if lottery_time[-1].text[-8:] == "a prize.":
-        chat = driver.find_element_by_css_selector("#chat-input")
+        chat = driver.find_element(By.CSS_SELECTOR, "#chat-input")
         chat.send_keys("lotto")
         time.sleep(1)
         chat.send_keys(Keys.RETURN)
@@ -30,29 +36,28 @@ def get_lottery_time():
 
 
 def play_minigame():
-    the_end = True
-    coins = int(driver.find_element_by_css_selector("#header_silver_count").text)
+    coins = int(driver.find_element(By.CSS_SELECTOR, "#header_silver_count").text)
+
     if coins >= 60:
         while coins > 0:
             time.sleep(1)
-            driver.find_element_by_css_selector(
-                "#header_content > div.header_game_wrapper > div.header_game.mini_dice"
-            ).click()
+            driver.find_element(By.CSS_SELECTOR,
+                                "#header_content > div.header_game_wrapper > div.header_game.mini_dice").click()
             time.sleep(random.randint(2, 3))
-            driver.find_element_by_css_selector("#dice_bet_auto").click()
+            driver.find_element(By.CSS_SELECTOR, "#dice_bet_auto").click()
             time.sleep(random.randint(2, 3))
-            driver.find_element_by_css_selector("#dice_auto_bet > input[type=number]").send_keys(f"{Keys.BACKSPACE}4")
+            driver.find_element(By.CSS_SELECTOR, "#dice_auto_bet > input[type=number]").send_keys(f"{Keys.BACKSPACE}4")
             time.sleep(random.randint(2, 3))
-            driver.find_element_by_css_selector("#dice_auto_start").click()
+            driver.find_element(By.CSS_SELECTOR, "#dice_auto_start").click()
             while coins > 0:
                 time.sleep(random.randint(1, 20))
-                coins = int(driver.find_element_by_css_selector("#header_silver_count").text)
+                coins = int(driver.find_element(By.CSS_SELECTOR, "#header_silver_count").text)
 
 
 def faucet_coin():
-    driver.find_element_by_css_selector("#ihm_faucet_button").click()
+    driver.find_element(By.CSS_SELECTOR, "#ihm_faucet_button").click()
     time.sleep(random.randint(1, 2))
-    driver.find_element_by_css_selector("#bonus_claim").click()
+    driver.find_element(By.CSS_SELECTOR, "#bonus_claim").click()
 
 
 def waiting_and_ending_driver():
@@ -61,15 +66,15 @@ def waiting_and_ending_driver():
 
 
 def get_login_and_password():
-    with open("login_and_password.json", "r") as f:
-        data = json.load(f)
-    login, password = data.values()
+    login = os.environ.get("nano_login")
+    password = os.environ.get("nano_password")
     return login, password
 
 
+load_dotenv(find_dotenv())
 prefs = {"credentials_enable_service": False, "profile.password_manager_enabled": False}
 chrome_options = Options()
-chrome_options.add_argument("--lang=pl")
+chrome_options.add_argument("--lang=en")
 chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_experimental_option(
@@ -80,10 +85,11 @@ login, password = get_login_and_password()
 while True:
     try:
         if platform.system() == "Linux":
-            driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options=chrome_options)
+            driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=chrome_options)
         elif platform.system() == "Windows":
+            s = Service(ChromeDriverManager().install())
             driver = webdriver.Chrome(
-                ChromeDriverManager().install(), chrome_options=chrome_options
+                service=s, chrome_options=chrome_options
             )
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
@@ -93,14 +99,14 @@ while True:
         driver.maximize_window()
         driver.implicitly_wait(10)
         time.sleep(1)
-        driver.find_element_by_css_selector("#ihm_login_button > span").click()
+        driver.find_element(By.CSS_SELECTOR, "#ihm_login_button > span").click()
         time.sleep(random.randint(1, 3))
-        driver.find_element_by_css_selector("#name").send_keys(login)
+        driver.find_element(By.CSS_SELECTOR, "#name").send_keys(login)
         time.sleep(random.randint(1, 3))
-        driver.find_element_by_css_selector("#pw").send_keys(password)
+        driver.find_element(By.CSS_SELECTOR, "#pw").send_keys(password)
         time.sleep(random.randint(1, 3))
-        driver.find_element_by_css_selector("#login_form_button > span").click()
-        send_notification("Logowanie poprawne!")
+        driver.find_element(By.CSS_SELECTOR, "#login_form_button > span").click()
+        send_notification("Login correct")
         time.sleep(random.randint(2, 4))
         get_lottery_time()
         time.sleep(random.randint(2, 4))
@@ -108,7 +114,7 @@ while True:
         time.sleep(random.randint(2, 4))
         play_minigame()
         time.sleep(1)
-        send_notification("Koniec")
+        send_notification("The End")
         waiting_and_ending_driver()
     except:
         driver.close()
---
+++ withdraw.py
@@ -0,0 +1,94 @@
+import time
+import random
+import os
+from imap_tools import MailBox, AND
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from dotenv import load_dotenv, find_dotenv
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import telepot
+import platform
+
+
+def get_email():
+    user = os.environ.get("email_login")
+    password_email = os.environ.get("email_password")
+    server = "imap.gmail.com"
+    mb = MailBox(server).login(user, password_email)
+    messages = mb.fetch(criteria=AND(seen=False, from_="luckynano.com"),
+                        mark_seen=True,
+                        bulk=True)
+
+    for msg in messages:
+        soup = BeautifulSoup(msg.html, 'html.parser')
+        el = soup.find(href=True)
+        url = el['href']
+        return url
+
+
+def send_notification(message):
+    api_bot = os.environ.get("api_bot")
+    chat_id = os.environ.get("chat_id")
+    bot = telepot.Bot(api_bot)
+    bot.sendMessage(chat_id, message)
+
+
+def get_login_and_password():
+    login = os.environ.get("nano_login")
+    password = os.environ.get("nano_password")
+    return login, password
+
+
+def make_withdraw():
+    driver.find_element_by_css_selector("#header_content > div.account_name").click()
+    time.sleep(random.randint(2, 4))
+    driver.find_element_by_css_selector("#withdraw_button").click()
+    time.sleep(random.randint(2, 4))
+    driver.find_element_by_css_selector("#withdraw_button_max").click()
+    time.sleep(random.randint(2, 4))
+    driver.find_element_by_css_selector("#withdraw_button_ok").click()
+
+load_dotenv(find_dotenv())
+prefs = {"credentials_enable_service": False, "profile.password_manager_enabled": False}
+chrome_options = Options()
+chrome_options.add_argument("--lang=pl")
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option(
+    "prefs", {"profile.managed_default_content_settings.javascript": 1}
+)
+chrome_options.add_experimental_option("useAutomationExtension", False)
+login, password = get_login_and_password()
+if platform.system() == "Linux":
+    driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options=chrome_options)
+elif platform.system() == "Windows":
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(
+        service=s, chrome_options=chrome_options
+    )
+driver.get("https://www.luckynano.com/")
+driver.maximize_window()
+driver.implicitly_wait(10)
+time.sleep(1)
+driver.find_element(By.CSS_SELECTOR, "#ihm_login_button > span").click()
+time.sleep(random.randint(1, 3))
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys(login)
+time.sleep(random.randint(1, 3))
+driver.find_element(By.CSS_SELECTOR, "#pw").send_keys(password)
+time.sleep(random.randint(1, 3))
+driver.find_element(By.CSS_SELECTOR, "#login_form_button > span").click()
+send_notification("Login correct")
+time.sleep(random.randint(2, 4))
+nano_amount = driver.find_element(By.CSS_SELECTOR, "#header_nano_count > span").text
+send_notification(f"Nano: {nano_amount}")
+make_withdraw()
+time.sleep(50)
+url = get_email()
+driver.get(url)
+time.sleep(10)
+driver.close()
